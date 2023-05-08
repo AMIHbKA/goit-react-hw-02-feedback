c6aa81ed10c9f17653fcf87b9f2a4d454be28097
AMIHbKA <bishop0013forum@gmail.com>
2023-05-08 08:55:41 +0300

    somecode

--- src/components/FeedbackWidget/FeedbackWidget.jsx
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -7,17 +7,53 @@ export class FeedbackWidget extends PureComponent {
     bad: 0,
   };
 
+  feedbackTotalCount = () => {
+    console.log(Object.values(this.state));
+    const values = this.state;
+    console.log(values);
+    // return Object.values(this.state).reduce((acc, cur) => acc + cur, 0);
+  };
+
+  positiveFeedbackPercentCount = ({ good }) => {
+    // return (this.feedbackTotalCount() / good) * 100;
+  };
+
+  feedbackButtonClickHandler = event => {
+    this.setState(prevState => {});
+    console.log('click on', event.target.name);
+  };
+
   render() {
     return (
       <>
         <h2>Please leave feedback</h2>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button
+          type="button"
+          name="good"
+          onClick={this.feedbackButtonClickHandler}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          name="neutral"
+          onClick={this.feedbackButtonClickHandler}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          name="bed"
+          onClick={this.feedbackButtonClickHandler}
+        >
+          Bad
+        </button>
         <h2>Statistics</h2>
-        <p>Good: 3</p>
-        <p>Neutral: 2</p>
-        <p>Bad:2</p>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad:{this.state.bad}</p>
+        <p>Total:{this.feedbackTotalCount}</p>
+        <p>Positive feedback:{this.positiveFeedbackPercentCount}</p>
       </>
     );
   }
